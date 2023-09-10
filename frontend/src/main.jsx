7b7817aa59f05f185f2623881fca332aa63cbe70
PRAMOD KUMAR MAURYA <115163168+indianpkm@users.noreply.github.com>
2023-09-10 11:17:11 +0530

    Add files via upload

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import DataProvider from "./context/DataContext.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BookDetail from "./component/singleBookDetail/BookDetail.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DataProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App/>} />
+          <Route path="/book/:id" element={<BookDetail/>}/>
+        </Routes>
+      </BrowserRouter>
+    </DataProvider>
+  </React.StrictMode>
+);
